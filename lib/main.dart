17c8711cd6b85b487728c522f5d1fc2b02c00ba5
Clifford <kingsleyclifford2016@gmail.com>
2021-11-20 18:03:44 +0000

    added lines of code

--- lib/main.dart
+++ lib/main.dart
@@ -14,30 +14,34 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.red[600],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.red,
-              child: const Text("Square 1"),
-              padding: const EdgeInsets.all(10.0),
-            ),
-            Container(
-              color: Colors.blue,
-              child: const Text("Square 2"),
-              padding: const EdgeInsets.all(20.0),
-            ),
-            Container(
-              color: Colors.green,
-              child: const Text("Square 3"),
-              padding: const EdgeInsets.all(30.0),
-            )
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.red,
+                child: const Text("Square 1"),
+                padding: const EdgeInsets.all(10.0),
+              ),
+              Container(
+                color: Colors.blue,
+                child: const Text("Square 2"),
+                padding: const EdgeInsets.all(20.0),
+              ),
+              Container(
+                color: Colors.green,
+                child: const Text("Square 3"),
+                padding: const EdgeInsets.all(30.0),
+              )
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          child: const Text('Click'),
+          child: const Icon(
+            Icons.add
+          ),
           backgroundColor: Colors.red[800],
         )
     );
